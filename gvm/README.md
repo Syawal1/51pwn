8a3871f28205e90439e1c196181c17556eb07bac
51pwn MTX <hktalent@qq.com>
2021-11-06 22:20:57 +0800

    x

--- gvm/README.md
+++ gvm/README.md
@@ -1,7 +1,24 @@
 OpenVAS (open vulnerability assessment system) is an excellent and open source vulnerability detection tool
 OpenVAS(Open Vulnerability Assessment System，开放式漏洞评估系统) 是一套优秀的、开源的漏洞检测工具
+@see https://docs.greenbone.net/GSM-Manual/gos-21.04/en/GSM-Manual-GOS-21.04-en.pdf
 # Run GVM（Greenbone Vulnerability Management）
 OpenVAS has been renamed GVM (greenbone vulnerability management) since version 10. The latest version of GVM v20.8.0 is used in this paper. Therefore, the word "OpenVAS" no longer appears and is uniformly called GVM
 OpenVAS从版本10开始，OpenVAS被改名为GVM（Greenbone Vulnerability Management），本文使用的是最新版的GVM v20.8.0，因此不再出现“OpenVAS”字样，统一叫GVM
+
+# How to: Reset admin password for OpenVAS and GVM 11
+<!-- https://dannyda.com/2020/08/26/how-to-reset-admin-password-for-openvas-and-gvm-11/?__cf_chl_captcha_tk__=xDh71zIdy_tf.E397Lakc6hPQO26.Q.gnvtfv1eUomA-1636207846-0-gaNycGzNB6U
+-->
+```
+docker exec -it gvm /bin/bash
+
+# list all user
+su gvm
+gvmd --get-users
+gvmd --get-roles
+gvmd --create-user=admin --role=Admin
+# change password
+gvmd --user=admin --new-password=admin
+```
+
 # Thanks
 @fnmsd
--- gvm/runGvm.sh
+++ gvm/runGvm.sh
@@ -3,7 +3,29 @@
 # macOS stop  postgresql
 brew services list
 brew services stop postgresql
+brew services stop redis
 
-docker run -d -p 2222:22 -v `pwd`/_data:/data  -p 5432:5432 -p 9392:9392 -p 6379:6379 -p 9390:9390 -e TZ="Asia/Shanghai" -e SSHD="true" -e DB_PASSWORD="dbpassword" -e PASSWORD="strongpassword" --name gvm jweny/gvm-docker-20.08
+docker stop gvm
+docker rm gvm
+docker run -d -p 2222:22 -v `pwd`/_data:/data -e USERNAME="admin" -e PASSWORD="admin" -p 5432:5432 -p 9392:9392 -p 6379:6379 -p 9390:9390 -e TZ="Asia/Shanghai" -e SSHD="true" -e DB_PASSWORD="dbpassword" -e PASSWORD="strongpassword" --name gvm jweny/gvm-docker-20.08
+# docker exec -it gvm /bin/bash
 docker start gvm
 docker logs -f gvm
+docker ps -a|grep gvm|grep 'Exited' && docker start gvm
+# docker exec -it gvm /bin/bash
+# su -c greenbone-feed-sync --type GVMD_DATA gvm
+# sh -c greenbone-feed-sync --type GVMD_DATA
+# /bin/sh /usr/local/sbin/greenbone-feed-sync --type GVMD_DATA
+# /usr/bin/rsync -ltvrP rsync://feed.community.greenbone.net:/data-objects/gvmd//timestamp /tmp/tmp.zB89PFE3y5
+docker logs -f gvm
+
+docker exec -it  gvm /bin/bash
+
+
+open https://127.0.0.1:9392
+
+docker stop scanner
+docker rm scanner
+docker run -d -v `pwd`/_data:/data -e MASTER_ADDRESS=docker.for.mac.localhost -e MASTER_PORT=2222 --name scanner jweny/gvm-scanner-docker-20.08
+docker ps -a|grep scanner|grep 'Exited' && docker start scanner
+docker logs -f scanner
